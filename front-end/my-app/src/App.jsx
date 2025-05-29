5689dad059e4b9bda29422cf3b65001daae967cc
amjadsh92 <amjadsharafeddien@gmail.com>
2025-05-29 13:37:32 +0300

    front-end:feat:Create the forms div

---
+++ front-end/my-app/src/App.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable */
+
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <body id="app" className="app">
+
+      <div id="forms" className="forms">
+        
+      </div>
+
+
+    </body>
+
+      
+    </>
+  )
+}
+
+export default App
