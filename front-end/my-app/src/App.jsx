fce770e5b55d7b747a5f92a750d4ec6bf5cf2f86
amjadsh92 <amjadsharafeddien@gmail.com>
2025-05-31 11:35:43 +0300

    front-end:feat:Change the syle using prime React

--- front-end/my-app/src/App.jsx
+++ front-end/my-app/src/App.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable */
 
+import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
 import { useState } from 'react'
+import { Card } from 'primereact/card';
 
 import './App.css'
 
@@ -9,27 +11,43 @@ function App() {
 
   return (
     <>
-    <body id="app" className="app">
+    <div id="app" className="app">
       <div id="title" className="title">
         Exercise Tracker
       </div>
+
       <div id="forms" className="forms">
 
-        <div id="addUserForm" className="addUserForm" >
+      
+
+        <div id="addUserForm" className="addUserForm">
+        <p className="user-title"> Create a new user</p>
+        <form id="user">
+        <label htmlFor="user-name" className="user-name-label">Username:</label>
+        <input id="user-name" type="text" className="user-name" />
+        <button type="submit" className="userSubmit">Submit</button>
+        </form>
 
         </div>
+
+        
+    
+
+      
+
+        
 
         <div id="addExerciseForm" className="addExerciseForm" >
           
         </div>
         <div id="showUserForm" className="showUserForm" >
-          
+
         </div>
 
       </div>
 
 
-    </body>
+    </div>
 
       
     </>
