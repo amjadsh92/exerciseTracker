4463e8e9a46f9978ae7a402f58b9715c05d13a12
amjadsh92 <amjadsharafeddien@gmail.com>
2025-06-02 13:55:16 +0300

    back-end:feat:handle adding a user to the datbase

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const { v4: uuidv4 } = require('uuid');
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
 
 app.use(cors())
 app.use(express.static('public'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
@@ -25,6 +28,7 @@ const pool = new Pool({
 const connectToDatabase = async () => {
   await pool.connect();
   console.log("Connected to Postgres database");
+  handleAPIs()
   listenToServer()
   
 };
@@ -36,5 +40,21 @@ const listener = app.listen(process.env.PORT || 3000, () => {
 })
 }
 
+
+const handleAPIs =() =>{
+
+
+  app.post("/api/users/", async function(req,res){
+    console.log(req.body)
+    const username = req.body.username
+    const id = uuidv4() 
+    const insertQuery = `INSERT INTO users(id,username) VALUES($1,$2)`
+    
+    const result  = await pool.query(insertQuery,[`${id}`,`${username}`] )
+
+    res.json({result: result})
+
+  })
+}
 
 connectToDatabase()
