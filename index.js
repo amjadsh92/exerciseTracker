0300665271a5f57f06a48a71fa84363baa09bb78
amjadsh92 <amjadsharafeddien@gmail.com>
2025-06-17 10:56:02 +0300

    make APIs suitable for fcc testing

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  database: process.env.DB_NAME
+  database: process.env.DB_FCC_NAME
 });
 
 const connectToDatabase = async () => {
@@ -49,12 +49,12 @@ const handleAPIs = () => {
     const usernameResult = await pool.query(selectUserQuery, [`${_id}`]);
     const usernameExists = usernameResult.rows.length;
 
-    if (!usernameExists) {
-      res
-        .status(400)
-        .json({ error: "This _id doesn't correspond to any username" });
-      return;
-    }
+    // if (!usernameExists) {
+    //   res
+    //     .status(400)
+    //     .json({ error: "This _id doesn't correspond to any username" });
+    //   return;
+    // }
 
     const username = usernameResult.rows[0].username;
 
@@ -66,41 +66,41 @@ const handleAPIs = () => {
       limit = limit.trim();
     }
 
-    if (startDate && isNaN(new Date(startDate).getTime())) {
-      res.status(400).json({ error: "The date is invalid" });
-      return;
-    }
+    // if (startDate && isNaN(new Date(startDate).getTime())) {
+    //   res.status(400).json({ error: "The date is invalid" });
+    //   return;
+    // }
 
-    if (startDate && startDate.match(/^-?\d+$/)) {
-      res.status(400).json({ error: "The date is invalid" });
-      return;
-    }
+    // if (startDate && startDate.match(/^-?\d+$/)) {
+    //   res.status(400).json({ error: "The date is invalid" });
+    //   return;
+    // }
 
-    if (endDate && isNaN(new Date(endDate).getTime())) {
-      res.status(400).json({ error: "The date is invalid" });
-      return;
-    }
+    // if (endDate && isNaN(new Date(endDate).getTime())) {
+    //   res.status(400).json({ error: "The date is invalid" });
+    //   return;
+    // }
 
-    if (endDate && endDate.match(/^-?\d+$/)) {
-      res.status(400).json({ error: "The date is invalid" });
-      return;
-    }
+    // if (endDate && endDate.match(/^-?\d+$/)) {
+    //   res.status(400).json({ error: "The date is invalid" });
+    //   return;
+    // }
 
-    if (startDate && endDate && startDate > endDate) {
-      res
-        .status(400)
-        .json({ error: "The start date can't be larger than the end date" });
-      return;
-    }
+    // if (startDate && endDate && startDate > endDate) {
+    //   res
+    //     .status(400)
+    //     .json({ error: "The start date can't be larger than the end date" });
+    //   return;
+    // }
 
-    if (limit && !limit.match(/^\d+$/)) {
-      res
-        .status(400)
-        .json({
-          error: "The number of exercises should be a positive integer",
-        });
-      return;
-    }
+    // if (limit && !limit.match(/^\d+$/)) {
+    //   res
+    //     .status(400)
+    //     .json({
+    //       error: "The number of exercises should be a positive integer",
+    //     });
+    //   return;
+    // }
 
     let query = `SELECT description, duration, date FROM exercises WHERE _id = $1`;
     const params = [_id];
@@ -137,32 +137,32 @@ const handleAPIs = () => {
   app.post("/api/users/", async function (req, res) {
     let username = req.body.username;
     username = username.trim();
-    if (!username) {
-      res.status(400).json({ error: "Can't enter an empty username!" });
-      return;
-    }
-    if (username.length >= 40) {
-      res
-        .status(400)
-        .json({
-          error: "The number of characters of the username can't exceed 40",
-        });
-      return;
-    }
+    // if (!username) {
+    //   res.status(400).json({ error: "Can't enter an empty username!" });
+    //   return;
+    // }
+    // if (username.length >= 40) {
+    //   res
+    //     .status(400)
+    //     .json({
+    //       error: "The number of characters of the username can't exceed 40",
+    //     });
+    //   return;
+    // }
     const selectUsername = `SELECT _id FROM users WHERE username=$1`;
     const selectResult = await pool.query(selectUsername, [`${username}`]);
     const usernameExists = selectResult.rows.length;
-    if (usernameExists) {
-      const _id = selectResult.rows[0]._id;
-      res
-        .status(400)
-        .json({
-          _id,
-          username: username,
-          error: `Username ${username} already exists.`,
-        });
-      return;
-    }
+    // if (usernameExists) {
+    //   const _id = selectResult.rows[0]._id;
+    //   res
+    //     .status(400)
+    //     .json({
+    //       _id,
+    //       username: username,
+    //       error: `Username ${username} already exists.`,
+    //     });
+    //   return;
+    // }
 
     const _id = uuidv4();
     const insertQuery = `INSERT INTO users(_id,username) VALUES($1,$2)`;
@@ -181,46 +181,46 @@ const handleAPIs = () => {
     let _id = req.params._id;
     let { description, duration, date } = req.body;
 
-    if (!description) {
-      res.status(400).json({ error: "Please provide a description" });
-      return;
-    }
+    // if (!description) {
+    //   res.status(400).json({ error: "Please provide a description" });
+    //   return;
+    // }
 
-    if (description.length >= 500) {
-      res
-        .status(400)
-        .json({
-          error:
-            "You exceeded the maximum number of characters allowed. The maximum number is 500",
-        });
-      return;
-    }
+    // if (description.length >= 500) {
+    //   res
+    //     .status(400)
+    //     .json({
+    //       error:
+    //         "You exceeded the maximum number of characters allowed. The maximum number is 500",
+    //     });
+    //   return;
+    // }
 
-    if (!duration) {
-      res.status(400).json({ error: "Please provide a duration" });
-      return;
-    }
+    // if (!duration) {
+    //   res.status(400).json({ error: "Please provide a duration" });
+    //   return;
+    // }
 
     duration = duration.trim().replace(/\s+/g, "");
     duration = Number(duration);
-    if (!Number.isInteger(duration) || duration <= 0) {
-      res.status(400).json({ error: "Duration must be a positive integer." });
-      return;
-    }
+    // if (!Number.isInteger(duration) || duration <= 0) {
+    //   res.status(400).json({ error: "Duration must be a positive integer." });
+    //   return;
+    // }
 
-    _id = _id.trim();
+    // _id = _id.trim();
 
     const selectQuery = `SELECT username FROM users WHERE _id=$1`;
     const result = await pool.query(selectQuery, [`${_id}`]);
     const usernameExists = result.rows.length;
-    if (!usernameExists) {
-      res
-        .status(400)
-        .json({
-          error: `The _id '${_id}' doesn't correspond to any username.`,
-        });
-      return;
-    }
+    // if (!usernameExists) {
+    //   res
+    //     .status(400)
+    //     .json({
+    //       error: `The _id '${_id}' doesn't correspond to any username.`,
+    //     });
+    //   return;
+    // }
 
     let username = result.rows[0].username;
     let submittedDate = new Date(date);
@@ -238,9 +238,6 @@ const handleAPIs = () => {
         currentDate,
       ]);
       res.json({ _id, description, duration, date: currentDate, username });
-    } else if (date.match(/^-?\d+$/) || !isValidDate) {
-      res.status(400).json({ error: `The date is invalid` });
-      return;
     } else {
       let formattedDate = submittedDate.toDateString();
       const insertQuery = `INSERT INTO exercises(_id,description, duration, date) VALUES($1,$2,$3,$4)`;
